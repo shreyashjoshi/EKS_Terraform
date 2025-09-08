67e54836d58f4f1e2e35d24e17f6f62735295cd2
shreyashjoshi <43450174+shreyashjoshi@users.noreply.github.com>
2025-09-08 20:13:56 +0530

    Update EKS.tf

--- EKS.tf
+++ EKS.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name               = "my-cluster"
+  name               = var.name
   kubernetes_version = "1.33"
 
   addons = {
